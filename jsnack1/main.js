b04fda7382e46aeb4cd7e2921aecbc07e8c87ec5
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-01-27 17:36:00 +0100

    algoritmo somma completato

---
+++ jsnack1/main.js
@@ -0,0 +1,26 @@
+/*
+
+Consegna:
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+*/
+
+let addendi = [];
+let somma = 0;
+const max = 10;
+
+let i = 0;
+do {
+    addendi[i] = parseInt(prompt('Inserisci ' + (i+1) + '° numero:'));
+    console.log(addendi[i]);
+    if (isNaN(addendi[i])) {
+        do {
+            addendi[i] = parseInt(prompt('Dato non corretto.\nInserisci ' + (i+1) + '° numero:'));
+        } while (isNaN(addendi[i]));
+    }
+    somma += addendi[i];
+    i++;
+} while (i < max);
+
+console.log(somma);
